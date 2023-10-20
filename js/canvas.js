9ee68cc5dd97498530c533e07301aee0c5a8d922
cristinang <cristinang20@gmail.com>
2023-10-20 13:35:49 +0200

    Lineadetiempo

---
+++ js/canvas.js
@@ -0,0 +1,53 @@
+var frame = window.requestAnimationFrame ||
+ 			window.mozRequestAnimationFrame || 
+			window.webkitRequestAnimationFrame || 
+			window.msRequestAnimationFrame;
+
+
+
+var canvas = document.querySelector("#lienzo");
+var ctx = canvas.getContext("2d");
+
+
+var numero = 0;
+var ubicacionX = 0;
+var animacion;
+
+
+
+//Dibujar SPRITE
+var sprite = new Image();
+sprite.src = "img/opcion1.png";
+
+// sprite.onload = function(){
+
+// 	//ctx.drawImage(imagen, ubicacionX, ubicacionY, recorteX, recorteY, x1,y1, x2, y2);
+// 	ctx.drawImage(sprite, 0, 0, 100, 100, 0, 100, 100, 100);
+
+// }
+
+function tiempo(){
+
+	if(numero >= 600){numero = 0}else{numero+=20}
+
+	for(var i = 0; i <= numero; i+=100){
+
+		ubicacionX = i;
+	}
+	// borrar el canvas
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	ctx.drawImage(sprite, ubicacionX, 0, 100, 100, 0, 100, 100, 100);
+
+	animacion = frame(tiempo);
+}
+
+tiempo();
+
+
+//Para detener la linea de tiempo - cancelAnimationFrame()
+
+setTimeout(function(){
+
+    cancelAnimationFrame(animacion)
+    
+    },3000)  // Paramos a los 3 segundos 
